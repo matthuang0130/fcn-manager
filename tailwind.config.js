46705e05da606f0152e2fd6f4f144f8c2727b90c
matthuang0130 <matthuang0130@gmail.com>
2026-08-06 20:21:29 +0800

    fix: 強制觸發 Vercel 重新編譯 Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // 🌟 加入這一行，讓系統支援手動切換深色模式
+  darkMode: 'class', // 🌟  加入這一行，讓系統支援手動切換深色模式
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
